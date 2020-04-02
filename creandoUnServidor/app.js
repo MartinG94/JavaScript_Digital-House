b6125df4aad9d53a23c681a0f4771a963410ca75
MartinG94 <guillenmartin94@gmail.com>
2020-04-02 15:22:13 -0300

    Routing

--- creandoUnServidor/app.js
+++ creandoUnServidor/app.js
@@ -4,9 +4,23 @@ const http = require('http');
 
 // Creación del servidor
 // Se encargará de manejar todas las peticiones
+// req tiene toda la información del request que vino desde el cliente
 
 http.createServer( function(req, res){
     res.writeHead(200, {'Content-Type':'text/plain'});
-    res.end('Mi primer server con Node');
+
+    switch(req.url){
+        case '/':
+            res.end('Bienvenido a mi Homepage!');
+            break;
+        case '/productos':
+            res.end('Estos son nuestros productos');
+            break;
+        default:
+            res.end('Error 404');
+            break;
+    };
+
+    res.end('Terminado');
 
 }).listen(3030, "localHost"); // Cualquier numero de 4 digitos
