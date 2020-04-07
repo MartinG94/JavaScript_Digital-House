ca8d1df1c83b0f5f49182e1cd4055c307ffd4e14
MartinG94 <guillenmartin94@gmail.com>
2020-04-07 14:59:13 -0300

    Funcion leerJSON()

--- EjercitacionNodeJS/logica/gestorFaqs.js
+++ EjercitacionNodeJS/logica/gestorFaqs.js
@@ -2,6 +2,12 @@ const fs = require('fs');
 
 const gestorFaqs = {
     rutaArchivo: './data/faqs.json',
+    leerJSON: function(){
+        let faqsJSON = fs.readFileSync(this.rutaArchivo, 'utf-8');
+        let faqsARRAY = JSON.parse(faqsJSON);
+        return faqsARRAY;
+    },
+    
 };
 
 module.exports = gestorFaqs;
--- EjercitacionNodeJS/logica/gestorMovies.js
+++ EjercitacionNodeJS/logica/gestorMovies.js
@@ -2,6 +2,12 @@ const fs = require('fs');
 
 const gestorMovies = {
     rutaArchivo: './data/movies.json',
+    leerJSON: function(){
+        let moviesJSON = fs.readFileSync(this.rutaArchivo, 'utf-8');
+        let moviesARRAY = JSON.parse(moviesJSON);
+        return moviesARRAY;
+    },
+    
 };
 
 module.exports = gestorMovies;
