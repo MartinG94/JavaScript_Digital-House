7ef97b3f23db52b714181b85acde64c4ac315449
MartinG94 <guillenmartin94@gmail.com>
2020-04-09 20:35:43 -0300

    Creado el objeto datosDeCartelera que se muestra en /en-cartelera

---
+++ EjercitacionNodeJS/src/enCartelera.js
@@ -0,0 +1,26 @@
+const movies = require('../data/movies');
+const cantidadDePeliculas = movies.length;
+const orderedMovies = movies.sort((movie1, movie2) => {
+    const movie1Title = movie1.title;
+    const movie2Title = movie2.title;
+    if (movie1Title < movie2Title) {
+      return -1;
+    }
+    if (movie1Title > movie2Title) {
+      return 1;
+    }
+    return 0;
+  });
+
+const pelisYSusReseñas = orderedMovies.map( (movie) => 
+`Titulo: ${movie.title}
+Reseña:${movie.overview}`
+);
+
+const datosDeCartelera = {
+    Titulo: 'En Cartelera',
+    'Total de peliculas': cantidadDePeliculas,
+    'Listado De Peliculas': pelisYSusReseñas,
+};
+
+module.exports = datosDeCartelera;
