d213a1582c558498c204aabd01cf015147050188
MartinG94 <guillenmartin94@gmail.com>
2020-04-09 20:39:58 -0300

    Formateo del Código

--- EjercitacionNodeJS/src/enCartelera.js
+++ EjercitacionNodeJS/src/enCartelera.js
@@ -1,26 +1,27 @@
-const movies = require('../data/movies');
+const movies = require("../data/movies");
 const cantidadDePeliculas = movies.length;
 const orderedMovies = movies.sort((movie1, movie2) => {
-    const movie1Title = movie1.title;
-    const movie2Title = movie2.title;
-    if (movie1Title < movie2Title) {
-      return -1;
-    }
-    if (movie1Title > movie2Title) {
-      return 1;
-    }
-    return 0;
-  });
+  const movie1Title = movie1.title;
+  const movie2Title = movie2.title;
+  if (movie1Title < movie2Title) {
+    return -1;
+  }
+  if (movie1Title > movie2Title) {
+    return 1;
+  }
+  return 0;
+});
 
-const pelisYSusReseñas = orderedMovies.map( (movie) => 
-`Titulo: ${movie.title}
+const pelisYSusReseñas = orderedMovies.map(
+  (movie) =>
+    `Titulo: ${movie.title}
 Reseña:${movie.overview}`
 );
 
 const datosDeCartelera = {
-    Titulo: 'En Cartelera',
-    'Total de peliculas': cantidadDePeliculas,
-    'Listado De Peliculas': pelisYSusReseñas,
+  Titulo: "En Cartelera",
+  "Total de peliculas": cantidadDePeliculas,
+  "Listado De Peliculas": pelisYSusReseñas,
 };
 
 module.exports = datosDeCartelera;
