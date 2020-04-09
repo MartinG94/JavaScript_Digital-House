55b85a47847fd9f25869406995fb9b60d8625068
MartinG94 <guillenmartin94@gmail.com>
2020-04-09 20:06:12 -0300

    Formateo del código

--- EjercitacionNodeJS/src/home.js
+++ EjercitacionNodeJS/src/home.js
@@ -1,30 +1,29 @@
-const movies = require('../data/movies');
+const movies = require("../data/movies");
 const moviesLength = movies.length;
-const orderedMovies = movie.sort(
-    (movie1, movie2) => {
-        const movie1Title = movie1.title;
-        const movie2Title = movie2.title;
 
-        if( movie1Title < movie2Title ){
-            return -1;
-        }
-        if( movie1Title > movie2Title){
-            return 1;
-        };
-        return 0;
+const orderedMovies = movies.sort((movie1, movie2) => {
+  const movie1Title = movie1.title;
+  const movie2Title = movie2.title;
+  if (movie1Title < movie2Title) {
+    return -1;
+  }
+  if (movie1Title > movie2Title) {
+    return 1;
+  }
+  return 0;
 });
 
 const datosDeInicio = {
-    Titulo: `Bienvenido a DH movies el mejor sitio para encontrar las mejores peliculas, Incluso mucho mejor que Netflix, Cuevana y PopCorn`,
-    'Total de Peliculas en Cartelera': moviesLength,
-    'Listado de peliculas': orderedMovies,
-    'Pie de pagina': [
-        'En Cartelera',
-        'Más Votadas',
-        'Sucursales',
-        'Contacto',
-        'Preguntas Frecuentes'
-    ],
-}
+  Titulo: `Bienvenido a DH movies el mejor sitio para encontrar las mejores peliculas, Incluso mucho mejor que Netflix, Cuevana y PopCorn`,
+  "Total de Peliculas en Cartelera": moviesLength,
+  "Listado de peliculas": orderedMovies,
+  "Pie de pagina": [
+    "En Cartelera",
+    "Más Votadas",
+    "Sucursales",
+    "Contacto",
+    "Preguntas Frecuentes",
+  ],
+};
 
 module.exports = datosDeInicio;
