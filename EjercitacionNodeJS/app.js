05007f029c5b94c655f1524aab25f3724970a43a
MartinG94 <guillenmartin94@gmail.com>
2020-04-11 12:14:41 -0300

    Se requiere el archivo de rutas

--- EjercitacionNodeJS/app.js
+++ EjercitacionNodeJS/app.js
@@ -1,47 +1,10 @@
 const http = require("http");
 const fs = require('fs');
-const moduleHome = require('./src/home');
-const moduleEnCartelera = require('./src/enCartelera');
-const moduleMasVotadas = require('./src/masVotadas');
+const rutas = require('./rutas');
 
 http
   .createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-
-    switch (req.url) {
-      case "/":
-        {
-			res.end(JSON.stringify(moduleHome));
-        }
-        break;
-      case "/en-cartelera":
-        {
-          res.end(JSON.stringify(moduleEnCartelera));
-        }
-        break;
-      case "/mas-votadas":
-        {
-          res.end(JSON.stringify(moduleMasVotadas));
-        }
-        break;
-      case "/sucursales":
-        {
-          res.end("Sucursales");
-        }
-        break;
-      case "/contacto":
-        {
-          res.end("Contacto");
-        }
-        break;
-      case "/preguntas-frecuentes":
-        {
-          res.end("Preguntas Frecuentes");
-        }
-        break;
-      default: {
-        res.end("404 not found");
-      }
-    }
+    rutas(req, res);
   })
   .listen(3030, "localhost", () => console.log("Server running in 3030 port"));
