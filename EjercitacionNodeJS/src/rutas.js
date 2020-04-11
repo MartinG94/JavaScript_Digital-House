d635bd2dae0c477e4586cbb4eee084868a87657a
MartinG94 <guillenmartin94@gmail.com>
2020-04-11 16:03:13 -0300

    Formateo

--- EjercitacionNodeJS/src/rutas.js
+++ EjercitacionNodeJS/src/rutas.js
@@ -1,46 +1,46 @@
-const moduleHome = require('./home');
-const moduleEnCartelera = require('./enCartelera');
-const moduleMasVotadas = require('./masVotadas');
-const moduleSucursales = require('./sucursales');
-const moduleContacto = require('./contacto');
-const modulePreguntasFrecuentes = require('./preguntasFrecuentes');
+const moduleHome = require("./home");
+const moduleEnCartelera = require("./enCartelera");
+const moduleMasVotadas = require("./masVotadas");
+const moduleSucursales = require("./sucursales");
+const moduleContacto = require("./contacto");
+const modulePreguntasFrecuentes = require("./preguntasFrecuentes");
 
-function ruteo(req, res){
-    switch (req.url) {
-        case "/":
-          {
-            res.end(JSON.stringify(moduleHome));
-          }
-          break;
-        case "/en-cartelera":
-          {
-            res.end(JSON.stringify(moduleEnCartelera));
-          }
-          break;
-        case "/mas-votadas":
-          {
-            res.end(JSON.stringify(moduleMasVotadas));
-          }
-          break;
-        case "/sucursales":
-          {
-            res.end(JSON.stringify(moduleSucursales));
-          }
-          break;
-        case "/contacto":
-          {
-            res.end(JSON.stringify(moduleContacto));
-          }
-          break;
-        case "/preguntas-frecuentes":
-          {
-            res.end(JSON.stringify(modulePreguntasFrecuentes));
-          }
-          break;
-        default: {
-          res.end("404 not found");
-        }
+function ruteo(req, res) {
+  switch (req.url) {
+    case "/":
+      {
+        res.end(JSON.stringify(moduleHome));
       }
-};
+      break;
+    case "/en-cartelera":
+      {
+        res.end(JSON.stringify(moduleEnCartelera));
+      }
+      break;
+    case "/mas-votadas":
+      {
+        res.end(JSON.stringify(moduleMasVotadas));
+      }
+      break;
+    case "/sucursales":
+      {
+        res.end(JSON.stringify(moduleSucursales));
+      }
+      break;
+    case "/contacto":
+      {
+        res.end(JSON.stringify(moduleContacto));
+      }
+      break;
+    case "/preguntas-frecuentes":
+      {
+        res.end(JSON.stringify(modulePreguntasFrecuentes));
+      }
+      break;
+    default: {
+      res.end("404 not found");
+    }
+  }
+}
 
 module.exports = ruteo;
