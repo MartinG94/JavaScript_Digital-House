6e31541c8d436c182ef234d25f84b840de37b6d4
MartinG94 <guillenmartin94@gmail.com>
2020-04-11 12:18:54 -0300

    Se agrega la variable promedios que contiene la list de promedios de las peliculas mas votadas y se utiliza para calcular el ratingPromedio

--- EjercitacionNodeJS/src/masVotadas.js
+++ EjercitacionNodeJS/src/masVotadas.js
@@ -1,15 +1,18 @@
 const movies = require("../data/movies");
 const moviesMasVotadas = movies.filter( pelicula => pelicula.vote_average > 7);
 const cantidadDePeliculasMasVotadas = moviesMasVotadas.length;
-const sumasDePromedios = (moviesMasVotadas) => {
+const promedios = moviesMasVotadas.map( unaPeli => {
+    return unaPeli.vote_average;
+});
+const sumasDePromedios = (listaDePromedios) => {
     let suma = 0;
-    for(let promedio of moviesMasVotadas){
+    for(let promedio of listaDePromedios){
         suma += promedio;
     }
     return suma;
 };
 
-const ratingPromedio = sumasDePromedios / cantidadDePeliculasMasVotadas;
+const ratingPromedio = sumasDePromedios(promedios) / cantidadDePeliculasMasVotadas;
 
 const listaPeliculas = moviesMasVotadas.map( pelicula => {
     return {'Titulo':pelicula.title, 'Rating':pelicula.vote_average, 'Reseña':pelicula.overview};
