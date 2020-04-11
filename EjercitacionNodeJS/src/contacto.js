89c6c0c62381a0cdac795227ffb9a91c806ad768
MartinG94 <guillenmartin94@gmail.com>
2020-04-11 12:42:52 -0300

    Archivo contacto.js

---
+++ EjercitacionNodeJS/src/contacto.js
@@ -0,0 +1,11 @@
+const emailDH = 'dhmovies@digitalhouse.com'
+
+const datosContacto = {
+    Titulo: 'Contáctanos',
+    Contenido1: '¿Tienes algo para nosotros? Nos encanta escuchar a nuestros clientes.',
+    Contenido2: `Si deseas contactarnos podés escribirnos al siguiente email ${emailDH} o en nuestras redes sociales`,
+    Contenido3: 'Envianos tu consulta, sugerencia o reclamo y será respondido a la brevedad',
+    Contenido4: 'Recordá que también podes consultar la sección Preguntas Frecuentes para obtener respuestas inmediatas a los problemas más comunes'
+};
+
+module.exports = datosContacto;
