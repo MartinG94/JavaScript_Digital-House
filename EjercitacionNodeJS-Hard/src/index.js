ddc9be90fb53b998b5b71717a2783e6179b16dab
MartinG94 <guillenmartin94@gmail.com>
2020-04-13 11:07:30 -0300

    Nueva clave en el objeto paginaWeb: preguntasFrecuentes

--- EjercitacionNodeJS-Hard/src/index.js
+++ EjercitacionNodeJS-Hard/src/index.js
@@ -3,13 +3,15 @@ const moduloCartelera = require('./enCartelera');
 const moduloMasVotadas = require('./masVotadas');
 const moduloSucursales = require('./sucursales');
 const moduloContacto = require('./contacto');
+const moduloPreguntasFrecuentes = require('./preguntasFrecuentes');
 
 const paginaWeb = {
     home: moduloHome,
     cartelera: moduloCartelera,
     masVotadas: moduloMasVotadas,
     sucursales: moduloSucursales,
-    contacto: moduloContacto
+    contacto: moduloContacto,
+    preguntasFrecuentes: moduloPreguntasFrecuentes
 };
 
 module.exports = paginaWeb;
