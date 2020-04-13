a5c7337071d4d3cff94220e67ac551188daba678
MartinG94 <guillenmartin94@gmail.com>
2020-04-13 11:01:38 -0300

    Nueva clave en el objeto paginaWeb: contacto

--- EjercitacionNodeJS-Hard/src/index.js
+++ EjercitacionNodeJS-Hard/src/index.js
@@ -2,12 +2,14 @@ const moduloHome = require('./home');
 const moduloCartelera = require('./enCartelera');
 const moduloMasVotadas = require('./masVotadas');
 const moduloSucursales = require('./sucursales');
+const moduloContacto = require('./contacto');
 
 const paginaWeb = {
     home: moduloHome,
     cartelera: moduloCartelera,
     masVotadas: moduloMasVotadas,
-    sucursales: moduloSucursales
+    sucursales: moduloSucursales,
+    contacto: moduloContacto
 };
 
 module.exports = paginaWeb;
