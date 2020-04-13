42c0eb68d65d409cb6e89178d64b40f37876b659
MartinG94 <guillenmartin94@gmail.com>
2020-04-13 09:52:43 -0300

    Nueva clave en el objeto paginaWeb, cartelera

--- EjercitacionNodeJS-Hard/src/index.js
+++ EjercitacionNodeJS-Hard/src/index.js
@@ -1,7 +1,9 @@
 const moduloHome = require('./home');
+const moduloCartelera = require('./enCartelera');
 
 const paginaWeb = {
-    home: moduloHome
+    home: moduloHome,
+    cartelera: moduloCartelera
 };
 
 module.exports = paginaWeb;
