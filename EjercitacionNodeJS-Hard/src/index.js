1bc3792d58d665286f99ac33cf5dfc13c0d78f35
MartinG94 <guillenmartin94@gmail.com>
2020-04-13 10:55:30 -0300

    Nueva clave en el objeto página web: sucursales

--- EjercitacionNodeJS-Hard/src/index.js
+++ EjercitacionNodeJS-Hard/src/index.js
@@ -1,11 +1,13 @@
 const moduloHome = require('./home');
 const moduloCartelera = require('./enCartelera');
 const moduloMasVotadas = require('./masVotadas');
+const moduloSucursales = require('./sucursales');
 
 const paginaWeb = {
     home: moduloHome,
     cartelera: moduloCartelera,
-    masVotadas: moduloMasVotadas
+    masVotadas: moduloMasVotadas,
+    sucursales: moduloSucursales
 };
 
 module.exports = paginaWeb;
