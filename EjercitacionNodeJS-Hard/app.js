91bd9c6c94321a326e0a3b3f1b1e1b0033aed1d8
MartinG94 <guillenmartin94@gmail.com>
2020-04-13 09:09:48 -0300

    app.js requiere a router para operar con req

--- EjercitacionNodeJS-Hard/app.js
+++ EjercitacionNodeJS-Hard/app.js
@@ -1,7 +1,7 @@
 const http = require('http');
+const router = require('./router');
 
 http.createServer((req, res) => {
 	res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-	// Route System
-	// ¿?
+	router(req, res);
 }).listen(3030, 'localhost', () => console.log('Server running in 3030 port'));
