067e1a56a939003b34c247177f6628e54590e993
MartinG94 <guillenmartin94@gmail.com>
2020-04-13 10:47:18 -0300

    Ultimos datos necesarios

--- EjercitacionNodeJS-Hard/src/masVotadas.js
+++ EjercitacionNodeJS-Hard/src/masVotadas.js
@@ -14,10 +14,19 @@ let sumarArray = (unArray) => {
     return resultado;
 };
 let ratingPromedio = Number((sumarArray(promediosDeLasMasVotadas) / cantidadPeliculasMasVotadas).toFixed(2));
+const listadoPeliculasMasVotadas = peliculasMasVotadas.map( (unaPelicula) => {
+    return {
+        'Titulo': unaPelicula.title,
+        'Rating': unaPelicula.vote_average,
+        'Reseña': unaPelicula.overview
+    };
+});
 
 const masVotadas = {
     Titulo: 'Más Votadas',
     'Total de películas': cantidadPeliculasMasVotadas,
     'Rating Promedio': ratingPromedio,
-    'Listado de películas':
+    'Listado de películas': listadoPeliculasMasVotadas
 };
+
+module.exports = masVotadas;
