527a355d98c14215801b80077a223cfb33939bf3
MartinG94 <guillenmartin94@gmail.com>
2020-04-13 10:48:29 -0300

    Nueva clave del objeto paginaWeb: masVotadas

--- EjercitacionNodeJS-Hard/src/index.js
+++ EjercitacionNodeJS-Hard/src/index.js
@@ -1,9 +1,11 @@
 const moduloHome = require('./home');
 const moduloCartelera = require('./enCartelera');
+const moduloMasVotadas = require('./masVotadas');
 
 const paginaWeb = {
     home: moduloHome,
-    cartelera: moduloCartelera
+    cartelera: moduloCartelera,
+    masVotadas: moduloMasVotadas
 };
 
 module.exports = paginaWeb;
