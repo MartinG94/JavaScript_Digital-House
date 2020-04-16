9186d61fc41b8b13db905fb11890931f2a79eec9
MartinG94 <guillenmartin94@gmail.com>
2020-04-16 16:18:27 -0300

    Simple procesamiento de distintos ids

--- ModuloExpress/app.js
+++ ModuloExpress/app.js
@@ -8,3 +8,19 @@ app.listen(3000, () => {
 app.get('/', (req, res) => {
     res.send('Hola Mundo! By Express');
 });
+
+// ruta parametrizada y genérica
+app.get('/productos/:idProducto', (req, res) => {
+    let id = req.params.idProducto;
+    res.send(`Bienvenidos al detalle del producto ${id}`);
+});
+
+app.get('/productos/:idProducto/comentarios/:idComentario?', (req, res) => {
+    let idProd = req.params.idProducto;
+    let idComent = req.params.idComentario;
+    if(idComent == undefined){
+        res.send(`Bienvenidos a los comentarios del producto ${idProd}`);
+        return;
+    };
+    res.send(`Bienvenidos a los comentarios del producto ${idProd} y enfocado en el comentario ${idComent}`);
+});
