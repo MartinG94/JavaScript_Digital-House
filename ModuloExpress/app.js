324e76762b9a26679c2026863725e47f23dd86f5
MartinG94 <guillenmartin94@gmail.com>
2020-04-16 15:29:19 -0300

    Levantando un servidor

--- ModuloExpress/app.js
+++ ModuloExpress/app.js
@@ -1,2 +1,6 @@
 const express = require('express');
 const app = express(); // Variable donde están disponibles todos los metodos del Framework
+
+app.listen(3030, () => {
+    console.log('Servidor en ejecución');
+});
